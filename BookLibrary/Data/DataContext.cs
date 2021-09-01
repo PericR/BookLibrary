8adc086f5d6ba3b275524e82e692d08666ada6b1
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-01 15:17:30 +0200

    Add project files.

---
+++ BookLibrary/Data/DataContext.cs
@@ -0,0 +1,18 @@
+﻿using BookLibrary.Entities;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Data
+{
+    public class DataContext : DbContext
+    {
+        public DataContext(DbContextOptions options) : base(options)
+        {
+        }
+
+        public DbSet<Book> Books { get; set; }
+    }
+}
