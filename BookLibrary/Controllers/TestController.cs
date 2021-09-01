732e19a3f8896cf51f2f7b11d0f3719b31f1cdfc
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-01 17:09:30 +0200

    Dummy test controller

---
+++ BookLibrary/Controllers/TestController.cs
@@ -0,0 +1,26 @@
+﻿using BookLibrary.DTOs;
+using BookLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    public class TestController : BaseApiController
+    {
+        private readonly IBookService _bookService;
+
+        public TestController(IBookService bookService)
+        {
+            _bookService = bookService;
+        }
+
+        /*[HttpGet]
+        public async Task<ActionResult<IEnumerable<BookDto>>> GetAllBooks()
+        {
+            return await _bookService.GetBooksAsync();
+        }*/
+    }
+}
