cdcbc9384d649ddd500af51e9c13dce9a687eda7
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-01 17:08:22 +0200

    Create AutoMapperProfiles.cs

---
+++ BookLibrary/Helpers/AutoMapperProfiles.cs
@@ -0,0 +1,18 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using AutoMapper;
+using BookLibrary.DTOs;
+using BookLibrary.Entities;
+
+namespace BookLibrary.Helpers
+{
+    public class AutoMapperProfiles : Profile
+    {
+        public AutoMapperProfiles()
+        {
+            CreateMap<Book, BookDto>();
+        }
+    }
+}
