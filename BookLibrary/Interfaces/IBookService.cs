94392109d394f44429f4a6b2937f9c083336b16b
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-01 17:09:13 +0200

    Added interface for BookService

---
+++ BookLibrary/Interfaces/IBookService.cs
@@ -0,0 +1,17 @@
+﻿using BookLibrary.DTOs;
+using BookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Interfaces
+{
+    public interface IBookService
+    {
+        void Update(Book book);
+        Task<bool> SaveAllAsync();
+        Task<IEnumerable<BookDto>> GetBooksAsync();
+        Task<Book> GetBookByIdAsync();
+    }
+}
