0d2df6e910cbbcefc919e936d133bf0dba00fd69
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-02 09:10:53 +0200

    Updated book dtos

--- BookLibrary/DTOs/AddBookDto.cs
+++ BookLibrary/DTOs/AddBookDto.cs
@@ -7,7 +7,6 @@ namespace BookLibrary.DTOs
 {
     public class AddBookDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
--- BookLibrary/DTOs/GetBookDto.cs
+++ BookLibrary/DTOs/GetBookDto.cs
@@ -7,7 +7,6 @@ namespace BookLibrary.DTOs
 {
     public class GetBookDto
     {
-        public int Id { get; set; }
         public string Name { get; set; }
         public string Author { get; set; }
         public string Genre { get; set; }
