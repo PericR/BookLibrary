f5cac816fc51399f59bd9a31d61dda0aa47fc3f9
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-03 06:55:58 +0200

    Invoice services and controller

---
+++ BookLibrary/Controllers/InvoiceController.cs
@@ -0,0 +1,51 @@
+﻿using BookLibrary.DTOs;
+using BookLibrary.Interfaces;
+using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Controllers
+{
+    public class InvoiceController : BaseApiController
+    {
+        private readonly IInvoiceService invoiceService;
+
+        public InvoiceController(IInvoiceService invoiceService)
+        {
+            this.invoiceService = invoiceService;
+        }
+
+        [HttpPost("new-invoice")]
+        public async Task<ActionResult> AddInvoice(AddInvoiceDto addInvoiceDto)
+        {
+            await this.invoiceService.AddInvoice(addInvoiceDto);
+
+            return Ok();
+        }
+
+        [HttpGet("get-by-book-id/{bookId}")]
+        public ActionResult<IEnumerable<GetInvoiceDto>> GetInvoicesByBookId(int bookId)
+        {
+            var invoices =  this.invoiceService.GetInvoiceByBookId(bookId);
+            
+            return Ok(invoices);
+        }
+
+        [HttpGet("get-by-user-id/{userId}")]
+        public ActionResult<IEnumerable<GetInvoiceDto>> GetInvoicesByBookId(string userId)
+        {
+            var invoices = this.invoiceService.GetInvoicesByUserId(userId);
+
+            return Ok(invoices);
+        }
+
+        [HttpGet("get-invoice/{id}")]
+        public ActionResult<GetInvoiceDto> GetInvoiceById(int id)
+        {
+            var invoice = this.invoiceService.GetInvoiceById(id);
+            return Ok(invoice);
+        }
+    }
+}
---
+++ BookLibrary/Services/AuthService.cs
@@ -0,0 +1,58 @@
+﻿using BookLibrary.DTOs;
+using BookLibrary.Entities;
+using BookLibrary.Interfaces;
+using Microsoft.AspNetCore.Identity;
+using Microsoft.Extensions.Configuration;
+using Microsoft.IdentityModel.Tokens;
+using System;
+using System.Collections.Generic;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using System.Security.Claims;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Services
+{
+    public class AuthService : IAuthService
+    {
+        private readonly UserManager<User> userManager;
+        private readonly IConfiguration config;
+        private readonly SymmetricSecurityKey key;
+
+        public AuthService(UserManager<User> userManager, IConfiguration config)
+        {
+            this.userManager = userManager;
+            this.config = config;
+            this.key = new SymmetricSecurityKey(Encoding.UTF8.GetBytes(config["TokenKey"]));
+        }
+
+        public string CreateToken(User user)
+        {
+            var claims = new List<Claim>
+            {
+                new Claim(JwtRegisteredClaimNames.NameId, user.UserName)
+            };
+
+            var creds = new SigningCredentials(this.key, SecurityAlgorithms.HmacSha512Signature);
+
+            var tokenDescriptor = new SecurityTokenDescriptor
+            {
+                Subject = new ClaimsIdentity(claims),
+                Expires = DateTime.Now.AddDays(1),
+                SigningCredentials = creds
+            };
+
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var token = tokenHandler.CreateToken(tokenDescriptor);
+
+            return tokenHandler.WriteToken(token);
+        }
+
+        public async Task<bool> ValidateUser(UserLoginDto userLoginDto)
+        {
+            User user = await this.userManager.FindByNameAsync(userLoginDto.UserName);
+            return (user != null && await this.userManager.CheckPasswordAsync(user, userLoginDto.Password));
+        }
+    }
+}
