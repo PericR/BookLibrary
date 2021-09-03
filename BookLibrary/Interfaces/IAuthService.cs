c91135d5ec3d6c536d29b9551061cd95ccd6303c
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-03 06:56:53 +0200

    Auth services and user dto

---
+++ BookLibrary/Interfaces/IAuthService.cs
@@ -0,0 +1,15 @@
+﻿using BookLibrary.DTOs;
+using BookLibrary.Entities;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace BookLibrary.Interfaces
+{
+    public interface IAuthService
+    {
+        Task<bool> ValidateUser(UserLoginDto userLoginDto);
+        string CreateToken(User user);
+    }
+}
--- BookLibrary/Startup.cs
+++ BookLibrary/Startup.cs
@@ -1,23 +1,10 @@
-using BookLibrary.Data;
-using BookLibrary.Entities;
 using BookLibrary.Extensions;
-using BookLibrary.Interfaces;
-using BookLibrary.Services;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
-using Microsoft.AspNetCore.HttpsPolicy;
-using Microsoft.AspNetCore.Identity;
-using Microsoft.AspNetCore.Mvc;
-using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using Microsoft.Extensions.Logging;
 using Microsoft.OpenApi.Models;
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
 
 namespace BookLibrary
 {
@@ -54,6 +41,8 @@ namespace BookLibrary
             app.UseHttpsRedirection();
 
             app.UseRouting();
+
+            app.UseAuthentication();
 
             app.UseAuthorization();
 
