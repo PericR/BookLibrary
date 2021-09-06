f2cd31a2c5a64cf751bc4a9e8b980d248bb7550d
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-06 09:18:41 +0200

    Basic components creation
    Added book component, register component.
    Added ngx-bootstrap to project.

---
+++ client/src/app/_services/books.service.ts
@@ -0,0 +1,16 @@
+import { HttpClient } from '@angular/common/http';
+import { Injectable } from '@angular/core';
+import { environment } from 'src/environments/environment';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class BooksService {
+  baseUrl = environment.apiUrl;
+  
+  constructor(private httpClient: HttpClient) { }
+
+  getBooks() {
+    return this.httpClient.get(this.baseUrl + 'book/books');
+  }
+}
