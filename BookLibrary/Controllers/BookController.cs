7b81b971151be34fb0e4a03a849d1a4288f53c52
Robert Peric <36766838+PericR@users.noreply.github.com>
2021-09-06 09:17:13 +0200

    Removed authorization for testing

--- BookLibrary/Controllers/BookController.cs
+++ BookLibrary/Controllers/BookController.cs
@@ -27,7 +27,7 @@ namespace BookLibrary.Controllers
             return Ok();
         }
 
-        [Authorize(Roles ="Visitor, Administrator")]
+        //[Authorize(Roles ="Visitor, Administrator")]
         [HttpGet("books")]
         public async Task<ActionResult<IEnumerable<GetBookDto>>> GetBooks()
         {
